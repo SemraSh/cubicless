4d9e46124711ac78c8826f5284eb18951050e66c
Semra Shukrieva <semra.shukrieva@yld.io>
2019-04-11 14:58:21 +0100

    change data exportation

--- dummyData.js
+++ dummyData.js
@@ -1,62 +1,56 @@
-const principles = [
-  {
-    name: 'Prin Ciple',
-  },
-  {
-    name: 'Ciple Prin',
-  },
-  {
-    name: 'Julie Ciple',
-  },
-  {
-    name: 'Quim Ciple',
-  },
-]
-
-const clients = [
-  {
-    name: 'Conde Nast',
-    status: 'active',
-  },
-  {
-    name: 'Elife',
-    status: 'inactive',
-  },
-  {
-    name: 'Nexmo',
-    status: 'active',
-  },
-  {
-    name: 'Kingfisher',
-    status: 'inactive',
-  },
-  {
-    name: 'Perform',
-    status: 'active',
-  },
-  {
-    name: 'Bench',
-    status: 'active',
-  },
-]
-
-const engineers = [
-  {
-    name: 'John Wayne',
-    level: 'I',
-  },
-  {
-    name: 'Engine Er',
-    level: 'III',
-  },
-  {
-    name: 'Some Engineer Name',
-    level: 'II',
-  },
-]
-
-export default {
-  principles,
-  clients,
-  engineers,
+module.exports = {
+  principles: [
+    {
+      name: 'Prin Ciple',
+    },
+    {
+      name: 'Ciple Prin',
+    },
+    {
+      name: 'Julie Ciple',
+    },
+    {
+      name: 'Quim Ciple',
+    },
+  ],
+  clients: [
+    {
+      name: 'Conde Nast',
+      status: 'active',
+    },
+    {
+      name: 'Elife',
+      status: 'inactive',
+    },
+    {
+      name: 'Nexmo',
+      status: 'active',
+    },
+    {
+      name: 'Kingfisher',
+      status: 'inactive',
+    },
+    {
+      name: 'Perform',
+      status: 'active',
+    },
+    {
+      name: 'Bench',
+      status: 'active',
+    },
+  ],
+  engineers: [
+    {
+      name: 'John Wayne',
+      level: 'I',
+    },
+    {
+      name: 'Engine Er',
+      level: 'III',
+    },
+    {
+      name: 'Some Engineer Name',
+      level: 'II',
+    },
+  ],
 }
